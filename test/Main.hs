d0b3c365f9d57972d43e4d5a51f04fc411f8b611
Dylan Simon <dylan@dylex.net>
2016-05-19 23:44:46 -0400

    Switch from QuickCheck to HUnit
    since we aren't using arbitrary

--- test/Main.hs
+++ test/Main.hs
@@ -4,8 +4,7 @@ module Main (main) where
 import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import System.Exit (exitSuccess, exitFailure)
-import qualified Test.QuickCheck as Q
-import Test.QuickCheck.Test (isSuccess)
+import qualified Test.HUnit as U
 
 import Web.Route.Invertible
 import Web.Route.Invertible.Host (splitHost)
@@ -59,21 +58,21 @@ req m p h = blankRequest
   , requestHost = splitHost h
   }
 
-tests :: Q.Property
-tests = Q.conjoin
-  [ routeRequest (req PUT ["thing", "09"] "") things Q.=== Right "put thing9"
-  , requestRoute (actionRoute postThing) "foo" Q.=== req POST ["thing", "foo"] ""
-  , lookupRoute (req OPTIONS ["thing", "xxx"] "") things Q.=== AllowedMethods [POST]
-  , routeRequest (req PUT ["thing", "0", "sub", "12", "34"] "") things Q.=== Right "thing0 sub 12 34"
-  , routeRequest (req GET ["thing", "0", "12", "34"] "") things Q.=== Right "ignore thing0"
-  , requestRoute (actionRoute ignoredThing) (-3) Q.=== req GET ["thing", "-3", "ign"] ""
-  , lookupRoute (req POST ["foo"] "foo.com") things Q.=== AllowedMethods [GET]
-  , lookupRoute (req POST ["foo"] "") things Q.=== RouteNotFound
+tests :: U.Test
+tests = U.test
+  [ routeRequest (req PUT ["thing", "09"] "") things U.~?= Right "put thing9"
+  , requestRoute (actionRoute postThing) "foo" U.~?= req POST ["thing", "foo"] ""
+  , lookupRoute (req OPTIONS ["thing", "xxx"] "") things U.~?= AllowedMethods [POST]
+  , routeRequest (req PUT ["thing", "0", "sub", "12", "34"] "") things U.~?= Right "thing0 sub 12 34"
+  , routeRequest (req GET ["thing", "0", "12", "34"] "") things U.~?= Right "ignore thing0"
+  , requestRoute (actionRoute ignoredThing) (-3) U.~?= req GET ["thing", "-3", "ign"] ""
+  , lookupRoute (req POST ["foo"] "foo.com") things U.~?= AllowedMethods [GET]
+  , lookupRoute (req POST ["foo"] "") things U.~?= RouteNotFound
   ]
 
 main :: IO ()
 main = do
-  r <- Q.quickCheckResult tests
-  if isSuccess r
+  r <- U.runTestTT tests
+  if U.errors r == 0 && U.failures r == 0
     then exitSuccess
     else exitFailure
