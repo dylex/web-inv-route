77cf800f74316c41bad0fbcca448d20d9fd864b2
Dylan Simon <dylan@dylex.net>
2021-04-19 16:36:32 -0400

    Add render functions (from waimwork)

---
+++ Web/Route/Invertible/Render.hs
@@ -0,0 +1,43 @@
+-- |Render requests in various ways
+module Web.Route.Invertible.Render
+  ( renderRequestBuilder
+  , renderUrlRequestBuilder
+  , renderHamletUrl
+  ) where
+
+import           Control.Arrow (second)
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Builder as B
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import           Network.HTTP.Types.URI (Query, encodePathSegments, renderQueryBuilder, simpleQueryToQuery, queryTextToQuery)
+
+import Web.Route.Invertible.Request
+import Web.Route.Invertible.Query
+import Web.Route.Invertible.Route
+
+-- |This renders a request along with additional query parameters as @[//host][/path][?query]@.
+-- Each component is included only if it's specified by the route.
+renderRequestBuilder :: Request -> Query -> B.Builder
+renderRequestBuilder r q =
+     bh (requestHost r)
+  <> bp (requestPath r)
+  <> renderQueryBuilder True ((simpleQueryToQuery $ paramsQuerySimple $ requestQuery r) ++ q)
+  where
+  bh [] = mempty
+  bh [x] = sl <> sl <> B.byteString x
+  bh (x:l) = bh l <> B.char8 '.' <> B.byteString x
+  bp [] = sl
+  bp p = encodePathSegments p
+  sl = B.char8 '/'
+
+-- |This renders a request along with additional query parameters as @scheme:[//host][/path][?query]@.
+-- It's roughly equivalent to rendering the 'Web.Route.Invertible.URI.requestURI'.
+renderUrlRequestBuilder :: Request -> Query -> B.Builder
+renderUrlRequestBuilder r q =
+  B.string8 (if requestSecure r then "https:" else "http:") <> renderRequestBuilder r q
+
+-- |A 'Text.Hamlet.Render' function, suitable for passing to a @'Text.Hamlet.HtmlUrl' (RouteAction a b, a)@ template.
+renderHamletUrl :: (RouteAction a b, a) -> [(T.Text, T.Text)] -> T.Text
+renderHamletUrl (r, a) q = TE.decodeUtf8 $ BSL.toStrict $ B.toLazyByteString
+  $ renderRequestBuilder (requestActionRoute r a) $ queryTextToQuery $ map (second Just) q
--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Data.Maybe (fromJust)
-import Network.URI (parseURI)
-import System.Exit (exitSuccess, exitFailure)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Builder as BSB
+import           Data.Maybe (fromJust)
+import           Network.URI (parseURI)
+import           System.Exit (exitSuccess, exitFailure)
 import qualified Test.HUnit as U
 
 import Web.Route.Invertible
 import Web.Route.Invertible.URI
+import Web.Route.Invertible.Render
 
 getThing :: RouteAction Int String
 getThing = routeMethod GET *< routePath ("thing" *< parameter) `RouteAction` \i ->
@@ -62,17 +65,23 @@ rte m u = lookupRoute (uriRequest m $ fromJust $ parseURI u) things
 req :: RouteAction r a -> r -> (Method, String)
 req a = fmap show . routeActionURI a
 
+ren :: RouteAction r a -> r -> BS.ByteString
+ren r a = BSB.toLazyByteString $ renderUrlRequestBuilder (requestActionRoute r a) []
+
 tests :: U.Test
 tests = U.test
   [ rte GET "http:/thing/0"           U.~?= RouteResult "get thing0"
   , rte GET "http:/thing/-3?type=x"   U.~?= RouteResult "get thing-3:x"
   , req getThingQ (98, '=')           U.~?= (GET, "http:/thing/98?type=%3D")
+  , ren getThingQ (98, '=')           U.~?= "http:/thing/98?type=%3D"
   , rte PUT "http:/thing/09"          U.~?= RouteResult "put thing9"
   , req postThing "foo"               U.~?= (POST, "http:/thing/foo")
+  , ren postThing "foo"               U.~?= "http:/thing/foo"
   , rte OPTIONS "http:/thing/xxx"     U.~?= AllowedMethods [POST]
   , rte PUT "http:/thing/0/sub/12/34" U.~?= RouteResult "thing0 sub 12 34"
   , rte GET "http:/things/0/12/34"    U.~?= RouteResult "ignore thing0"
   , req ignoredThing (-3)             U.~?= (GET, "http:/things/-3/ign")
+  , ren ignoredThing (-3)             U.~?= "http:/things/-3/ign"
   , rte POST "http://foo.com/foo"     U.~?= AllowedMethods [GET]
   , rte POST "http:/foo"              U.~?= RouteNotFound
   ]
